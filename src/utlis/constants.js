ddd4abc23a228046fc5d25ac6e0ed7ad9a5931b5
kumawat mahendra <kumawatmahendra7896@gmail.com>
2026-08-02 15:17:45 +0530

    remove api key

--- src/utlis/constants.js
+++ src/utlis/constants.js
@@ -1,7 +1,8 @@
 const HEYTUBE_API_KEY = "AIzaSyBcbqPctzBYepRlBU_8zPjVXxZ0eIogPbE"
 
-export const OFFSET_LIVE_CHAT = 400
+export const OFFSET_LIVE_CHAT = 180
 
 export const HEYTUBE_VIDEO_API = "https://www.googleapis.com/youtube/v3/videos?part=snippet,contentDetails,statistics&chart=mostPopular&regionCode=IN&maxResults=50&key=AIzaSyBcbqPctzBYepRlBU_8zPjVXxZ0eIogPbE";
 
 export const YOUTUBE_SEARCH_API = "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q="
+
